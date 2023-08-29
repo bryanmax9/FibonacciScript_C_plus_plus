36575fe8b2be416b0e5b56001e005ef82298bcc2
Bryan Tineo <brillant16.gcv@gmail.com>
2023-08-28 21:38:12 -0700

    First c++ script

---
+++ fibo.cpp
@@ -0,0 +1,22 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+//Fibo function
+int fibo (int n)
+{
+	if (n == 1 || n == 0)
+		return 1;
+	else
+		return fibo(n-1) + fibo(n-2); //recursion
+}
+
+int main()
+{
+	for (int i = 0; i <= 20;i++)
+		cout << i << ":" << fibo(i)<<endl;
+
+	return 0;
+}
